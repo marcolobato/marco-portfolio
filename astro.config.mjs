b4711c7f68dedf2b6ff0f7b4d0bd56c7da91617d
Marco Lobato <marcolobato.ux@gmail.com>
2026-03-25 21:55:59 -0600

    Experiments page now uses accordions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
 import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
 
   // mdx() lets .mdx files use Astro components inside Markdown.
   // alpinejs() injects Alpine.js globally on every page.
-  integrations: [mdx(), alpinejs()]
+  integrations: [mdx(), alpinejs(), icon()]
 });
